970eb624aa013a1dccf60610b50a2566cbff67ad
marycottrill <21mcottrill@athenian.org>
2020-11-03 18:12:35 +0000

    Ninth Grade Class

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,26 @@
-public class NinthGrader {
+public class NinthGrader extends Student {
+
+    //Instance variables already inherited 
+
+    //Constructors 
+    public NinthGrader(String a, String b){
+        super(a, b, 2024); 
+    }
+
+    public NinthGrader(){ //defualt no argument constructor 
+        super("John", "Smith", 2024); 
+        //can also do this: super();
+    }
+
+    //doSomething() method
+    public void doSomething() {
+        System.out.println("I'm doing my homework.");
+    }
+
+    //toString() method 
+    public String toString() {
+        String str = this.getFirstName() + " " + this.getLastName() + ", " + this.getClassYear() + " (Ninth Grader)";
+        return str;
+    }
     
 }
