b2a6251a2e1eb72a568bc6fae78799886f5d3ba5
marycottrill <21mcottrill@athenian.org>
2020-11-05 08:08:13 +0000

    Grocery List Code

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,10 +1,46 @@
 public class GroceryList {
+    
+    //Instance variables
     private String[] arr; 
 
+    //methods 
     public GroceryList(){
         this.arr = new String[10];
     }
     
-    if (arr[i] == null);//add
+    //add method 
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
 
+    //remove method 
+    public void remove(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            int totheleft = arr.length - (i+1);
+            if (arr[i] == item) {
+                for (int x = 1; x <= totheleft; x++)
+                    arr[i + (x-1)] = arr[i + x];
+                break;    
+            }
+        }
+    }
+
+    //toString method *ask about this. 
+    public String toString(){
+        String str = "Grocery List: ";
+        for (int i = 0; i < arr.length; i++) {
+            str = str + arr[i];
+            if (i != arr.length){
+                str = str + ", ";
+            }
+        }
+        return str;
+    }
+
+    
 }
